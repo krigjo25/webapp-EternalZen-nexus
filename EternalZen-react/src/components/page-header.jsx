f49712f15fa0c13b67955238191c1a3315a54eb7
krigjo25 <krigjo25@gmail.com>
2025-04-08 10:24:44 +0200

    IC

--- EternalZen-react/src/components/page-header.jsx
+++ EternalZen-react/src/components/page-header.jsx
@@ -243,11 +243,11 @@ function affirmation()
 
 function login()
 {
-    return `<form>
+    return (`<form>
     <input type="text" name="username" placeholder="Username" required>
     <input type="password" name="password" placeholder="Password" required>
     <button type="submit">Login</button>
-    </form>`;
+    </form>`);
 }
 
 export default Header
