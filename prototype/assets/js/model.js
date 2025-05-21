d752e46bfac2b0c031a2ca85d843e6f4f5bde815
Kristoffer <krigjo25@gmail.com>
2025-05-21 13:31:07 +0200

    Initializing a prototype

---
+++ prototype/assets/js/model.js
@@ -0,0 +1,21 @@
+//  Application model
+let model = {
+    app:
+    {
+        name: "EternalZen",
+        version: "0.0.1beta",
+        currentPage: "index",
+        currentRoute: "index",
+        
+        logo: 
+        {
+            src: "assets/images/logo.png",
+            alt: "EternalZen Logo",
+        }
+    },
+    data:
+    {
+        user:
+        []
+    }
+}
