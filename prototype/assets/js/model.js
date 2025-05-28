7c8263511d25077b9d72e74c076fa528f7ba5267
Kristoffer <krigjo25@gmail.com>
2025-05-28 14:12:16 +0200

    modifying the model's navigation system,

--- prototype/assets/js/model.js
+++ prototype/assets/js/model.js
@@ -4,17 +4,97 @@ let model = {
     {
         name: "EternalZen",
         version: "0.0.1beta",
-        currentPage: "index",
+        currentPage: "about",
         currentRoute: "index",
-        
+
+    },
+    header:
+    {
+        title: this.app,
+        subtitle: "Your personal meditation and affirmation app",
         logo: 
         {
             src: "assets/images/logo.png",
             alt: "EternalZen Logo",
-        }
+        },
+        nav:
+        {
+            cls: "flex-wrap-row",
+            links:
+            [
+                {
+                    type: "link",
+                    name: "Login",
+                    link: "#login",
+                },
+                {
+                    type: "link",
+                    name: "Register", 
+                    link: "#register",
+                },
+            ],
+        },
     },
+
     data:
     {
+        routes:
+        [
+            {
+                id: 0,
+                name: "index",
+                nav:
+                {
+                    cls: "flex-wrap-row-justify-space-evenly",
+                    links: 
+                    [
+                        {
+                            id: 0,
+                            type: "btn",
+                            name: "EternalZen?",
+                            function: "navigateView('about');",
+                        },
+                        {
+                            id: 1,
+                            type: "btn",
+                            name: "What Is Meditation?",
+                            function: "navigateView('wim');",
+                        },
+                        {
+                            id: 2,
+                            type: "btn",
+                            name: "What Is Affirmation?",
+                            function: "navigateView('wia');",
+                        },
+
+                    ],
+                },
+
+                pages:
+                [
+                    {
+                        id: 0,
+                        name: "index",
+                    },
+                    {
+                        id: 1,
+                        name: "about",
+                    },
+                    {
+                        id: 2,
+                        name: "WIA",
+                    },
+                    {
+                        id: 3,
+                        name: "login",
+                    },
+                    {
+                        id: 4,
+                        name: "register",
+                    },
+                ]
+            }
+        ],
         user:
         []
     }
