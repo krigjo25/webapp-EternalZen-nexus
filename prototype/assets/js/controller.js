b75d5ceb64ff7439499e98066fbb0ce768506f1e
Kristoffer <krigjo25@gmail.com>
2025-06-18 14:04:18 +0200

    Refactor form generation and credential verification functions

--- prototype/assets/js/controller.js
+++ prototype/assets/js/controller.js
@@ -30,8 +30,61 @@ function updateView()
             break;
     }
 
-    
     document.querySelector("#root").innerHTML = html;
     document.querySelector("#header").innerHTML = headerView();
     document.querySelector("#footer").innerHTML = footerView();
+}
+
+function generateForm(id)
+{
+    console.log("Generating form for id:", id);
+    const form = model.forms[id];
+    const inputs = form.inputs;
+
+    let html = /*HTML*/ `
+        <form onsubmit="handleLogin(event)" method="${form.method}" class="${form.cls}">`;
+
+    for (let i = 0; i < inputs.length; i++)
+    {
+        const input = inputs[i];
+
+        html += /*HTML*/ `
+                <label for="${input.id}">${input.name}</label>
+                <input type="${input.type ? input.type : 'text'}" id="${input.id}" name="${input.name}" placeholder="${input.placeholder}" class="${input.cls}" required="${input.required ? 'required' : ''}">`;
+                
+    }
+    html += /*HTML*/ `
+    <button type="${form.btn.type}" class="${form.btn.cls}">${form.btn.name}</button>
+    </form>`;
+    return html;
+
+}
+
+function verifyCredentials(e)
+{
+    //    #   Verify the credentials of the existing user
+
+    const data = model.data;
+    const user = data.users.find(user => user.username === e.target[0].value);
+
+    if (user.passcode !== e.target[1].value && user.username !== e.target[0].value)
+    {
+        //  Show an error message
+        alert("You submitted incorrect passcode or username try again.");
+        return;
+    }
+    else
+    {
+        //  Set the loggedIn variable to true
+        user.isActive = true;
+        updateView();
+        return;
+    }
+}
+
+function handleLogin(e)
+{
+        //  Prevent the page from refreshing
+        e.preventDefault();
+        verifyCredentials(e);
 }
